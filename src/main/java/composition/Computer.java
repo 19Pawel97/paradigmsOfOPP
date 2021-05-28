0aab04d3284a2d7633955c0850f8ca67733bc6a3
19Pawel97 <19pawel9772@gmail.com>
2021-05-28 17:23:40 +0200

    Composition 2.0

--- src/main/java/composition/Computer.java
+++ src/main/java/composition/Computer.java
@@ -1,2 +1,84 @@
-package composition;public class Computer {
+package composition;
+
+public class Computer {
+    private Processor processor;
+    private Ram ram;
+
+    public Computer(Processor processor, Ram ram) {
+        this.processor = processor;
+        this.ram = ram;
+    }
+
+    public void run() {
+        System.out.println("running");
+    }
+
+    public Processor getProcessor() {
+        return processor;
+    }
+
+    public void setProcessor(Processor processor) {
+        this.processor = processor;
+    }
+
+    public Ram getRam() {
+        return ram;
+    }
+
+    public void setRam(Ram ram) {
+        this.ram = ram;
+    }
+}
+
+
+class Processor {
+    String name;
+    int numberOfCores;
+
+    public Processor(String name, int numberOfCores) {
+        this.name = name;
+        this.numberOfCores = numberOfCores;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfCores() {
+        return numberOfCores;
+    }
+
+    public void setNumberOfCores(int numberOfCores) {
+        this.numberOfCores = numberOfCores;
+    }
+}
+
+class Ram {
+    String name;
+    int size;
+
+    public Ram(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
--- src/main/java/composition/Main.java
+++ src/main/java/composition/Main.java
@@ -1,2 +1,14 @@
-package composition;public class Main {
+package composition;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Computer computer = new Computer(new Processor("processor",4), new Ram("ram",16));
+
+        System.out.println(computer.getProcessor().getName());
+        System.out.println(computer.getRam().getName());
+
+    }
+
 }
